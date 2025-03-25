a3ccfe83791069ee41a6bb8b9e4abe0cade2bd9c
vinhstn <vinhstn@gmail.com>
2025-03-25 09:40:31 -0700

    Update SearchSort.java

--- SearchSort.java
+++ SearchSort.java
@@ -55,11 +55,11 @@ public class SearchSort{
         
            for (int i = 0; i < arr.length; i++){
                
-             if (???????? == ???????) {
-              return ????????;
+             if (arr[i] == target) {
+              return i;
              }
            }
-            return ???????;
+            return -1;
         }
         
         
@@ -70,23 +70,23 @@ public class SearchSort{
         {
             int left = 0;
             int right = arr.length - 1;
-            while (??????? <= ???????) {
-                int mid = (left + right) / ???????;
+            while (left <= right) {
+                int mid = (left + right) / 2;
      
                 
-                if (arr[??????] == ???????) {
-                    return ????????;
+                if (arr[mid] == target) {
+                    return mid;
      
-                } else if (arr[???????] > ????????) {
-                    right = ?????????;
+                } else if (arr[mid] > target) {
+                    right = mid-1;
      
                 } else {
-                  left = ?????????;
+                  left = mid+1;
                 }  
             }
      
         
-            return ????????;
+            return -1;
         }
         
         
@@ -98,16 +98,17 @@ public class SearchSort{
         {
             for(int i = 0; i < arr.length - 1; i++)
             {
-                int minIndex = ????????;
+                int minIndex = i;
                 
-                for(int j = ????????; j < arr.length; j++){
-                    if(arr[j] < arr[??????????])
-                        minIndex = ????????;
+                for(int j = i; j < arr.length; j++){
+                    if(arr[j] < arr[minIndex])
+                        minIndex = j;
                 }
                 
-                int temp = ????????;
-                arr[minIndex] = ???????;
-                arr[i] = ????????;
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+                
             }
         }
         
@@ -119,18 +120,19 @@ public class SearchSort{
         //the array is now sorted
             public static void insertionSort(int[] arr)
         {
+            
             for (int i = 1; i < arr.length; i++)
             {
-                int elementToInsert = arr[???????];
+                int elementToInsert = arr[i];
                 int j = i;
         
-                while (j > 0 && ????????? < arr[j - 1])
+                while (j > 0 && elementToInsert < arr[j - 1])
                 {
-                    arr[j] = arr[j - 1];
-                    ?????;
+                    arr[j] = arr[j-1];
+                    j = j-1;
                 }
         
-                arr[j] = ????????;
+                arr[j] = elementToInsert;
             }
         }
         
